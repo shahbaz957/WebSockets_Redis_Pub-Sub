e94c72d36d61c6d8e8a39b6fd8ed5bd0610586e2
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2026-01-15 16:42:30 +0500

    Minimal Chatting application is developed with client server communication via WS

--- Client_React/src/App.tsx
+++ Client_React/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [latestMessage, setLatestMessage] = useState<string>("");
+  const [message , setMessage] = useState<string>("");
+  const [sentMsg , setSentMsg] = useState<string>("")
 
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8002");
+    ws.onopen = () => {
+      console.log("Connected to the WS Server");
+      setSocket(ws);
+    };
+    ws.onmessage = (msg) => {
+      console.log("Recieved message : ", msg.data);
+      setLatestMessage(msg.data);
+    };
+    return () => {
+      ws.close();
+    }
+  }, []);
+
+  const handleSend = () => {
+    if (socket && message.trim() != ''){
+      socket.send(message)
+      setSentMsg(message);
+      setMessage("");
+    }
+  }
+
+  if (!socket) {
+    return <div>Connecting to the Websocket server ...</div>;
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <input type="text" onChange={(e) => setMessage(e.target.value)}/>
+    <button onClick={handleSend}>Send</button>
+      <div>{latestMessage}</div>
+      <h1>Clients Message : {sentMsg}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
