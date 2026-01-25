98f386874b195ca181723e540e192eb15aaf4895
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2026-01-25 22:17:57 +0500

    feat: add autocannon for benchmarking and update dependencies
    - Added autocannon as a dependency for performance benchmarking.
    - Created a new benchmark.js file to run benchmarks against the product endpoint.
    - Updated package.json and package-lock.json to include new dependencies and versions.

--- Redis/app.js
+++ Redis/app.js
@@ -1,38 +1,63 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
+import { createClient } from "redis";
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/node_cache');
+const client = createClient()
+  .on("error", (err) => console.log("ERROR : ", err))
+  .connect();
+
+mongoose.connect("mongodb://127.0.0.1:27017/node_cache");
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    price: Number,
-    category: String,
-    specs: Object,
+  name: String,
+  description: String,
+  price: Number,
+  category: String,
+  specs: Object,
 });
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
-app.get('/product' ,async (req , res) => {
-    // console.log(req.path)
-    // console.log(req.query)
-    // const params = req.query;
-    // // Object.keys(params).map((key)=> {
-    // //     console.log("KEY -> " , key , "VALUE -> " , params[key]);
-    // // })
-    // Object.values(params).map((value) => {
-    //     console.log("VALUE : " , value);
-    // })
+app.get("/product", async (req, res) => {
+  const key = generateCacheKey(req);
+  const cachedProducts = await client.get(key);
+  if (cachedProducts) {
+    console.log("Cache HIT");
+    res.json(JSON.parse(cachedProducts));
+    return;
+  }
+  console.log("Cache Miss");
+  const query = {};
+  if (req.query.category) {
+    query.category = req.query.category;
+  }
+  const products = await Product.find(query);
+  if (products.length) {
+    await client.set(key, JSON.stringify(products));
+  }
+  return res.json(products);
+});
 
-    const query = {};
-    if (req.query.category){
-        query.category = req.query.category;
-    }
-    const products = await Product.find(query);
+function generateCacheKey(req) {
+  const baseUrl = req.path.replace(/^\/+|\/+$/g, "").replace(/\//g, ":"); // replace all the / with :
+  // for /product/api ---> product:api
+  const params = req.query;
+  const sortedParams = Object.keys(params)
+    .sort()
+    .map((key) => `${key}=${params[key]}`)
+    .join("&");
 
-    return res.json(products);
-})
+  return sortedParams ? `${baseUrl}:${sortedParams}` : baseUrl;
+}
 
+app.listen(4000, () => console.log("Server is listening at PORT 4000"));
 
-
-app.listen(4000, () => console.log("Server is listening at PORT 4000"))
+// console.log(req.path)
+// console.log(req.query)
+// const params = req.query;
+// // Object.keys(params).map((key)=> {
+// //     console.log("KEY -> " , key , "VALUE -> " , params[key]);
+// // })
+// Object.values(params).map((value) => {
+//     console.log("VALUE : " , value);
+// })
